311b5228515d0d98375baf3e7a15322f713a67ea
Carlie Duperval <carlierichell@gmail.com>
2022-05-24 21:17:55 -0400

    Added MenuBar

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -7,20 +7,16 @@ import { UserContext } from '../App';
 const { Item } = Menu;
 
 export default function Menubar() {
-let navigate = useNavigate()
-const { user } = useContext(UserContext)
+let navigate = useNavigate();
+const { user } = useContext(UserContext);
 return (
     <Menu theme="dark" mode="horizontal">
-        
     <Item key="home" onClick={() => navigate('/')}
         icon={<HomeOutlined style={{ fontSize: '1.8em' }} />} />
-
     <Item key="add" onClick={() => navigate('/add')}
         icon={<PlusCircleOutlined style={{ fontSize: '1.8em' }} />} />
-
     {user && <Item key="random"
         icon={<QuestionCircleOutlined style={{ fontSize: '1.8em' }} />} />}
-
     </Menu>
 )
 }
