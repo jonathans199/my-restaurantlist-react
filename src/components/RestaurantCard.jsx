6cc03f3c8fec4d9e29935361262ad2d8b5f92e46
Carlie Duperval <carlierichell@gmail.com>
2022-05-24 21:19:31 -0400

    Added RrestaurantCards

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -2,14 +2,16 @@ import React, { useContext} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Col, Card, Rate} from 'antd';
 import { UserContext } from '../App';
-
 const { Meta } = Card;
 
 export default function RestaurantCard({ restaurant }) {
   let navigate = useNavigate();
   const { user }= useContext(UserContext);
   return (
-    <Col style={{ width: '300px', margin: '1em' }}>
+    <Col style={{ 
+      width: '300px', 
+      margin: '1em' 
+      }}>
       <Card
         loading={!restaurant}
         onClick={() => navigate(`/restaurants/${restaurant.id}`)}
